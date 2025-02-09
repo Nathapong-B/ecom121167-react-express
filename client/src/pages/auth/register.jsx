85cebfcec634d2cfdd17b29e89293c2e7c3a45e4
PoPKuB <btf_01@hotmail.com>
2025-02-09 17:15:27 +0700

    ui regis signin update

--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -65,8 +65,13 @@ export default function Register() {
     }, [watch().password]);
 
     return (
-        <div className="m-auto min-w-max w-96 h-max p-10 rounded rounded-md bg-gray-300/80">
+        <div className="relative m-auto min-w-max w-96 h-max p-10 rounded rounded-md bg-gray-300/80">
             <div className="w-full min-w-60">
+                <Link to={'/'}>
+                    <div className="absolute top-2 left-2 font-bold text-10px text-gray-400 bg-gray-200 px-2 py-1 rounded rounded-full text-center contents-center cursor-pointer hover:text-gray-200 hover:bg-sky-500">
+                        HOME
+                    </div>
+                </Link>
 
                 <div className="text-center font-bold text-4xl text-gray-500 pb-6">Register</div>
 
@@ -104,7 +109,7 @@ export default function Register() {
                 </form>
 
                 <div className="text-end">
-                    <span className="me-1 text-xs">or</span>
+                    <span className="me-1 text-xs italic">or</span>
                     <Link to={'/auth/signin'}>
                         <button className="inline hover:text-sky-500">Sign-in</button>
                     </Link>
